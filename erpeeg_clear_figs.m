1d1c45f451299b1b4e778fd08450ad1c5281c21b
BenSchwartzmann <33909069+BenSchwartzmann@users.noreply.github.com>
2018-03-09 15:09:20 -0800

    Delete erpeeg_clear_figs.m

--- erpeeg_clear_figs.m
+++
@@ -1,9 +0,0 @@
-function erpeeg_clear_figs()
-
-main_fig = findobj('type','figure','name','erpeeg');
-set(main_fig,'HandleVisibility','off');
-close all;
-set(main_fig,'HandleVisibility','on');
-
-end
-
